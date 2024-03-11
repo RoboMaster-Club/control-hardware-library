48abd1e73229fc7e5d090d0cd8900b190d35a813
Leo Liu <65527170+CuboiLeo@users.noreply.github.com>
2024-03-11 17:38:52 -0400

    some bug

--- BSP/Inc/bsp_serial.h
+++ BSP/Inc/bsp_serial.h
@@ -7,10 +7,10 @@
 
 #define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
 
-PUTCHAR_PROTOTYPE
+int _write(int file, char *ptr, int len)
 {
-    HAL_UART_Transmit(&huart6, (uint8_t *)&ch, 1, HAL_MAX_DELAY);
+    HAL_UART_Transmit(&huart6, (uint8_t *)ptr, len, HAL_MAX_DELAY);
     //while(!huart6.Instance->SR & UART_FLAG_IDLE);
-    return ch;
+    return len;
 }
 #endif
